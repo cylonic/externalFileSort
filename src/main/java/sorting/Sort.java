da5cb3eb24ed6940ee254e5fedaa12d2ca494ea2
weturner <turn0477@gmail.com>
2017-07-27 23:35:48 -0600

    added functionality to split one file into many sorted files - rudimentary

--- src/main/java/sorting/Sort.java
+++ src/main/java/sorting/Sort.java
@@ -1,84 +1,33 @@
 package sorting;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import io.FileWriter;
 import model.Item;
-import model.Item.ItemType;
 
-/**
- * 
- * Temp testing file
- *
- */
 public class Sort
 {
-    public static void main( String[] args )
+
+    public static List<Item<?>> sortList( List<Item<?>> items, boolean ascending )
     {
-        // sort();
-        itemTest();
+        return getSortedStream( items, ascending ).collect( Collectors.toList() );
     }
 
-    public Sort()
+    private static Stream<Item<?>> getSortedStream( List<Item<?>> list, boolean ascending )
     {
-        // TODO Auto-generated constructor stub
-    }
-
-    private static void itemTest()
-    {
-        Item<String> item = new Item<>( "99500", ItemType.STRING );
-        Item<String> item2 = new Item<>( "99595", ItemType.STRING );
-
-        System.out.println( item.compareTo( item2 ) );
-
-        System.out.println( "99500".compareTo( "99595" ) );
-    }
-
-    public static void sort()
-    {
-        List<Integer> ints = new ArrayList<>();
-
-        for (int i = 0; i < 10_000; i++)
-        {
-            ints.add( ThreadLocalRandom.current().nextInt( 1_000_000 ) );
-        }
-
-        ints = getStream( ints, true ).collect( Collectors.toList() );
-
-        FileWriter fw = new FileWriter( "/data/fluffy.txt" );
-
-        for (Integer line : ints)
-        {
-            try
-            {
-                fw.write( line.toString() );
-            } catch ( IOException e )
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        fw.close();
-
-    }
-
-    public static Stream<Integer> getStream( List<Integer> list, boolean ascending )
-    {
+        Comparator<Item<?>> c;
 
         if ( ascending )
-
         {
-            return list.stream().sorted();
+            c = ( i1, i2 ) -> i1.compareTo( i2 );
+        } else
+        {
+            c = ( i1, i2 ) -> i2.compareTo( i1 );
         }
 
-        return list.stream().sorted( Comparator.reverseOrder() );
+        return list.stream().sorted( c );
 
     }
 
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -10,12 +10,19 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
+import datasource.Datasource;
 import factory.DoubleItemFactory;
 import factory.IntegerItemFactory;
 import factory.ItemFactory;
 import factory.StringItemFactory;
+import io.FileWriter;
+import model.Item;
 import model.Item.ItemType;
+import sorting.Sort;
 
 public class Util
 {
@@ -94,6 +101,105 @@ public class Util
             }
 
         }
+    }
+
+    public static void main( String[] args )
+    {
+        splitFile( "/data/fluffy.txt" );
+        // generateFile();
+    }
+
+    public static void splitFile( String file )
+    {
+        Datasource<?> ds = new Datasource<>( file, ItemType.INTEGER );
+        List<String> files = new ArrayList<>();
+        List<Item<?>> items = new ArrayList<>();
+
+        String path = file.substring( 0, file.lastIndexOf( "." ) ) + "_sorted_";
+
+        System.out.println( path );
+
+        int fileCount = 1;
+        String nextFile = path + fileCount + ".txt";
+        FileWriter writer = new FileWriter( nextFile );
+        files.add( nextFile );
+
+        Item<?> item;
+        try
+        {
+            while ( ( item = ds.getNextItem() ) != null )
+            {
+                items.add( item );
+                // writer.write( item.getItem().toString() );
+
+                if ( items.size() >= 100_000 )
+                {
+                    writeAndSortList( items, writer );
+                    items.clear();
+                    writer.close();
+                    nextFile = path + ( ++fileCount ) + ".txt";
+                    writer = new FileWriter( nextFile );
+                    files.add( nextFile );
+                }
+
+            }
+        } catch ( IOException e )
+        {
+            e.printStackTrace();
+        } finally
+        {
+            if ( items.size() > 0 )
+            {
+                writeAndSortList( items, writer );
+            }
+            if ( writer != null )
+            {
+                writer.close();
+            }
+            System.out.println( files );
+        }
+    }
+
+    private static void writeAndSortList( List<Item<?>> items, FileWriter writer )
+    {
+        // nice n ugly
+        Sort.sortList( items, true ).stream().forEach( s -> {
+            try
+            {
+                writer.write( s.getItem().toString() );
+            } catch ( IOException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } );
+    }
+
+    public static void generateFile()
+    {
+        List<Item<?>> list = new ArrayList<>();
+
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            list.add( new Item<>( ThreadLocalRandom.current().nextInt( 1_000_000 ), ItemType.INTEGER ) );
+        }
+
+        // list = Sort.sortList( list, true );
+
+        FileWriter fw = new FileWriter( "/data/fluffy.txt" );
+
+        for (Item<?> line : list)
+        {
+            try
+            {
+                fw.write( line.getItem().toString() );
+            } catch ( IOException e )
+            {
+                e.printStackTrace();
+            }
+        }
+
+        fw.close();
     }
 
 }
